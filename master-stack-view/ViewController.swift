67817a923efd0aa75ed1420e5401a600cf7c7402
rikutech <rikutech@gmail.com>
2020-02-26 17:42:06 +0900

    Initial Commit

---
+++ master-stack-view/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  master-stack-view
+//
+//  Created by 今陸成 on 2020/02/26.
+//  Copyright © 2020 今陸成. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ master-stack-viewTests/master_stack_viewTests.swift
@@ -0,0 +1,34 @@
+//
+//  master_stack_viewTests.swift
+//  master-stack-viewTests
+//
+//  Created by 今陸成 on 2020/02/26.
+//  Copyright © 2020 今陸成. All rights reserved.
+//
+
+import XCTest
+@testable import master_stack_view
+
+class master_stack_viewTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
